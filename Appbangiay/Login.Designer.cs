918608d8ffe284f416ed47a49828a5c11aa5ea99
anhbaonguyenle <anhlenguyenbaoanh@gmail.com>
2023-04-01 18:36:17 +0700

    Login Function succession

--- Appbangiay/Login.Designer.cs
+++ Appbangiay/Login.Designer.cs
@@ -28,12 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAdmin = new System.Windows.Forms.TextBox();
+            this.txtAdminPass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(166, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng Nhập";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên đăng nhập";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mật Khẩu";
+            // 
+            // txtAdmin
+            // 
+            this.txtAdmin.Location = new System.Drawing.Point(195, 143);
+            this.txtAdmin.Name = "txtAdmin";
+            this.txtAdmin.Size = new System.Drawing.Size(255, 20);
+            this.txtAdmin.TabIndex = 3;
+            // 
+            // txtAdminPass
+            // 
+            this.txtAdminPass.Location = new System.Drawing.Point(195, 212);
+            this.txtAdminPass.Name = "txtAdminPass";
+            this.txtAdminPass.Size = new System.Drawing.Size(255, 20);
+            this.txtAdminPass.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Đăng Nhập";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(509, 339);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtAdminPass);
+            this.Controls.Add(this.txtAdmin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
             this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAdmin;
+        private System.Windows.Forms.TextBox txtAdminPass;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Appbangiay/Login.cs
+++ Appbangiay/Login.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,43 @@ namespace Appbangiay
 {
     public partial class Login : Form
     {
+        private SqlDataReader dr;
+
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void button1_Click(object sender, EventArgs e)
+        {
+            if (txtAdmin.Text != string.Empty || txtAdminPass.Text != string.Empty)
+            {
+                SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-Q056MBAS\SQLEXPRESS;Initial Catalog=QLGiay;Integrated Security=True");
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("select * from Login where USERNAME ='" + txtAdmin.Text + "' and password='" + txtAdminPass.Text + "';", conn);
+                dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    dr.Close();
+                    MessageBox.Show("Login successful ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //Nua thay doan nay thanh mo trang chu nhe Chi Bao :3
+                }
+                else
+                {
+                    dr.Close();
+                    MessageBox.Show("No Account avilable with this username and password ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Please enter value in all field.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
