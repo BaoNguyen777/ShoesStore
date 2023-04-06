5a373a66e543ca3f20777f4cc98de20f7180a7b1
anhbaonguyenle <anhlenguyenbaoanh@gmail.com>
2023-04-06 23:25:13 +0700

    Add Change color when select button on navigation control

--- Appbangiay/Home.cs
+++ Appbangiay/Home.cs
@@ -14,10 +14,15 @@ namespace Appbangiay
     public partial class Home : Form
     {
         NavigationControl navigationControl;
+        NavigationButtons navigationButtons;
+
+        Color btnDefailtColor = Color.FromKnownColor(KnownColor.Navy);
+        Color btnSelectColor = Color.FromKnownColor(KnownColor.OrangeRed);
         public Home()
         {
             InitializeComponent();
             InitializeNavigationControl();
+            InitializeNavigationButton();
         }
         private void InitializeNavigationControl()
         {
@@ -27,6 +32,16 @@ namespace Appbangiay
             navigationControl = new NavigationControl(userControls, panel1);
             navigationControl.Display(0);
         }
+
+        private void InitializeNavigationButton()
+        {
+            List<Button> buttons = new List<Button>()
+            { btnKho, btnHoaDon, btnDoanhThu, btnNhanVien};
+            navigationButtons = new NavigationButtons(buttons, btnDefailtColor, btnSelectColor);
+            navigationButtons.Highlight(btnKho);
+
+        }
+
         private void DonHangTab_Click(object sender, EventArgs e)
         {
 
@@ -45,26 +60,31 @@ namespace Appbangiay
         private void btnKho_Click(object sender, EventArgs e)
         {
             navigationControl.Display(0);
+            navigationButtons.Highlight(btnKho);
         }
 
         private void btnHoaDon_Click(object sender, EventArgs e)
         {
             navigationControl.Display(1);
+            navigationButtons.Highlight(btnHoaDon);
         }
 
         private void btnDoanhThu_Click(object sender, EventArgs e)
         {
             navigationControl.Display(2);
+            navigationButtons.Highlight(btnDoanhThu);
         }
 
         private void btnNhanVien_Click(object sender, EventArgs e)
         {
             navigationControl.Display(3);
+            navigationButtons.Highlight(btnNhanVien);
         }
 
         private void btnLogo_Click(object sender, EventArgs e)
         {
             navigationControl.Display(0);
+            navigationButtons.Highlight(btnKho);
         }
     }
 }
---
+++ Appbangiay/NavigationButtons.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Appbangiay
+{
+    public class NavigationButtons
+    {
+        List<Button> buttons;
+        Color defailtColor;
+        Color selectColor;
+        public NavigationButtons(List<Button> buttons, Color defailtColor, Color selectColor)
+        {
+            this.buttons = buttons;
+            this.defailtColor = defailtColor;
+            this.selectColor = selectColor;
+            SetButtonColor();
+        }
+        private void SetButtonColor()
+        {
+            foreach (Button button in buttons) 
+            {
+                button.BackColor = defailtColor;
+            }
+        }
+        public void Highlight(Button selectButton)
+        {
+            foreach (Button button in buttons)
+            {
+                if (button == selectButton)
+                {
+                    selectButton.BackColor = selectColor; 
+                }
+                else
+                {
+                    button.BackColor = defailtColor;
+                }
+            }
+        }
+    }   
+}
