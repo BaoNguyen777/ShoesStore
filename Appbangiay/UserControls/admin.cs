1b8b89da1edd703c5582047edd238bb6d673e94a
anhbaonguyenle <anhlenguyenbaoanh@gmail.com>
2023-04-12 08:34:36 +0700

    admin xong

--- Appbangiay/UserControls/admin.cs
+++ Appbangiay/UserControls/admin.cs
@@ -18,6 +18,7 @@ namespace Appbangiay.UserControls
             InitializeComponent();
             dataNV.DataSource = getAllNhanvien().Tables[0];
             //dataNV.DataMember = "nhanvien";
+            dataNV.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
         }
         DataSet getAllNhanvien()
         {
@@ -35,7 +36,14 @@ namespace Appbangiay.UserControls
         }
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow dr = this.dataNV.Rows[e.RowIndex];
+                txtSDT.Text = dr.Cells["Số điện thoại nhân viên"].Value.ToString();
+                txtTen.Text = dr.Cells["Tên Nhân viên"].Value.ToString();
+                txtChucvu.Text = dr.Cells["Chức vụ"].Value.ToString();
+                txtLuong.Text = dr.Cells["Lương"].Value.ToString();
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -57,6 +65,28 @@ namespace Appbangiay.UserControls
         private void button1_Click(object sender, EventArgs e)
         {
             string query = "Insert into nhanvien (nvsdt, nvmatkhau, nvten, nvchucvu, nvluong) values ('"+txtSDT.Text+"','"+txtMatKhau.Text+"','"+txtTen.Text+"','"+txtChucvu.Text+"',"+txtLuong.Text+");";
+            using (SqlConnection conn = new SqlConnection(connectionString.con))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.ExecuteReader();
+                dataNV.DataSource = getAllNhanvien().Tables[0];
+                conn.Close();
+
+            }
+        }
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            DataGridViewRow dr = dataNV.SelectedRows[0];
+            txtSDT.Text = dr.Cells[0].Value.ToString();
+            txtTen.Text = dr.Cells[1].Value.ToString();
+            txtChucvu.Text = dr.Cells[2].Value.ToString();
+            txtLuong.Text = dr.Cells[3].Value.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string query = "DELETE FROM nhanvien WHERE nvsdt = '"+txtSDT.Text+"';";
             using (SqlConnection conn = new SqlConnection(connectionString.con))
             {
                 conn.Open();
