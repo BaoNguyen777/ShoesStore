974e7a7bc209a23e5835f68a8f817bacbcc91c2a
anhbaonguyenle <anhlenguyenbaoanh@gmail.com>
2023-04-12 08:49:33 +0700

    Gan xong hoadon

--- Appbangiay/UserControls/HoaDon.Designer.cs
+++ Appbangiay/UserControls/HoaDon.Designer.cs
@@ -28,33 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.dtdxl = new System.Windows.Forms.DataGridView();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dtChuaxuly = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtdxl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtChuaxuly)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // dtdxl
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(334, 250);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(214, 25);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Giao Dien Hoa Don";
+            this.dtdxl.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtdxl.Location = new System.Drawing.Point(14, 42);
+            this.dtdxl.Name = "dtdxl";
+            this.dtdxl.Size = new System.Drawing.Size(955, 216);
+            this.dtdxl.TabIndex = 0;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(73, 312);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(128, 37);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Đã xử lý";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(406, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Không xử lý";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(777, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Sửa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dtChuaxuly
+            // 
+            this.dtChuaxuly.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtChuaxuly.Location = new System.Drawing.Point(14, 416);
+            this.dtChuaxuly.Name = "dtChuaxuly";
+            this.dtChuaxuly.Size = new System.Drawing.Size(955, 216);
+            this.dtChuaxuly.TabIndex = 4;
             // 
             // HoaDon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtChuaxuly);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.dtdxl);
             this.Name = "HoaDon";
             this.Size = new System.Drawing.Size(992, 661);
+            ((System.ComponentModel.ISupportInitialize)(this.dtdxl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtChuaxuly)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dtdxl;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dtChuaxuly;
     }
 }
--- Appbangiay/UserControls/HoaDon.cs
+++ Appbangiay/UserControls/HoaDon.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,81 @@ namespace Appbangiay.UserControls
         public HoaDon()
         {
             InitializeComponent();
+            dtdxl.DataSource = getHoaDondaxuly().Tables[0];
+            //dataNV.DataMember = "nhanvien";
+            dtdxl.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dtChuaxuly.DataSource = getAllHoaDon().Tables[0];
+            dtChuaxuly.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+        DataSet getAllHoaDon()
+        {
+            string query = "SELECT * from hoadon where tinhtrang ='Chua xu ly';";
+            DataSet data = new DataSet();
+            using (SqlConnection conn = new SqlConnection(connectionString.con))
+            {
+                conn.Open();
+
+                SqlDataAdapter adpter = new SqlDataAdapter(query, conn);
+                adpter.Fill(data);
+                conn.Close();
+            }
+            return data;
+        }
+        DataSet getHoaDondaxuly()
+        {
+            string query = "SELECT * from hoadon where tinhtrang ='Da xu ly';";
+            DataSet data = new DataSet();
+            using (SqlConnection conn = new SqlConnection(connectionString.con))
+            {
+                conn.Open();
+
+                SqlDataAdapter adpter = new SqlDataAdapter(query, conn);
+                adpter.Fill(data);
+                conn.Close();
+            }
+            return data;
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            string query = "DELETE FROM nhanvien WHERE nvsdt = '" + txtSDT.Text + "';";
+            using (SqlConnection conn = new SqlConnection(connectionString.con))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.ExecuteReader();
+                dataNV.DataSource = getAllNhanvien().Tables[0];
+                conn.Close();
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "DELETE FROM nhanvien WHERE nvsdt = '" + txtSDT.Text + "';";
+            using (SqlConnection conn = new SqlConnection(connectionString.con))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.ExecuteReader();
+                dataNV.DataSource = getAllNhanvien().Tables[0];
+                conn.Close();
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string query = "DELETE FROM nhanvien WHERE nvsdt = '" + txtSDT.Text + "';";
+            using (SqlConnection conn = new SqlConnection(connectionString.con))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.ExecuteReader();
+                dataNV.DataSource = getAllNhanvien().Tables[0];
+                conn.Close();
+
+            }
         }
     }
 }
